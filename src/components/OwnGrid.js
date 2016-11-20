1cc7b6899b8935f89bdd9fd7dcfe36fd12563256
davidank <dandahmann@gmail.com>
2016-11-20 11:45:48 -0800

    Init ownGrid

---
+++ src/components/OwnGrid.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Row from './Row';
+
+class OwnGrid extends Component {
+  render() {
+    return (
+      <div className='ownGrid'>
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+      </div>
+    );
+  }
+}
+
+export default OwnGrid;
---
+++ src/components/Row.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Box from './Box';
+
+class Row extends Component {
+  render() {
+    return (
+      <div className='row'>
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </div>
+    );
+  }
+}
+
+export default Row;
