66403ce37a69b083a1804943be7b8f2230ea9de7
Louis Bergeron <louispvb@gmail.com>
2016-12-04 12:49:21 -0800

    Remove underscore use lodash fp in client files

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Row from './Row';
-import _ from 'underscore';
+import _ from 'lodash/fp';
 
 class Grid extends Component {
   // constructor(props) {
@@ -10,12 +10,12 @@ class Grid extends Component {
     return (
       <div className='grid'>
         <div className='gridTitle'>{this.props.title}</div>
-        { _(10).times(i => <Row
-          key={i}
-          playerTurn={this.props.playerTurn}
-          row={this.props.matrix[i]}
-          rowNum={i}
-          submitPos={this.props.submitPos} />) }
+        { _.times(i => 
+          <Row key={i}
+               playerTurn={this.props.playerTurn}
+               row={this.props.matrix[i]}
+               rowNum={i}
+               submitPos={this.props.submitPos} />, 10) }
       </div>
     );
   }
--- client/src/components/Row.js
+++ client/src/components/Row.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import Box from './Box';
-import _ from 'underscore';
+import _ from 'lodash/fp';
 
 class Row extends Component {
   render() {
     return (
       <div className='row'>
-        { _(10).times(i => <Box
-          key={i}
-          playerTurn={this.props.playerTurn}
-          status={this.props.row[i]}
-          pos={[i,this.props.rowNum]}
-          submitPos={this.props.submitPos}
-        />) }
+        { _.times(i =>
+            <Box key={i}
+                 playerTurn={this.props.playerTurn}
+                 status={this.props.row[i]}
+                 pos={[i,this.props.rowNum]}
+                 submitPos={this.props.submitPos}/>, 10) }
       </div>
     );
   }
